279e4105b1de583699d13b2bf4e94a64723ffe43
Saopheng <saophengthaii@gmail.com>
2019-02-03 22:46:24 +0700

    Updates

---
+++ precache-manifest.7069b83be23f7e896c95fecdb4d31eda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4e5c756d9e8babc7374",
+    "url": "/reactjs/static/js/runtime~main.a4e5c756.js"
+  },
+  {
+    "revision": "cef7e457ad5bb82726f2",
+    "url": "/reactjs/static/js/main.cef7e457.chunk.js"
+  },
+  {
+    "revision": "54b8fe8dc458491e7e1f",
+    "url": "/reactjs/static/js/1.54b8fe8d.chunk.js"
+  },
+  {
+    "revision": "cef7e457ad5bb82726f2",
+    "url": "/reactjs/static/css/main.948eaa6c.chunk.css"
+  },
+  {
+    "revision": "54b8fe8dc458491e7e1f",
+    "url": "/reactjs/static/css/1.e108b306.chunk.css"
+  },
+  {
+    "revision": "8e8b0718beb2ecdd111f6e0dde58d1e8",
+    "url": "/reactjs/index.html"
+  }
+];
